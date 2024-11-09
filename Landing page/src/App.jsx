2837e7460ed3be5502c91f8f9a48ef69c6083140
Md Mohsin <108623805+mdmohsin6@users.noreply.github.com>
2024-11-09 11:01:16 +0530

    Add files via upload

---
+++ Landing page/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+const App = () =>{
+
+  return <>
+   <Navbar />
+   <Body />
+   </>
+}
+
+export default App;
---
+++ Landing page/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+
+const Navbar =() =>{
+    return (
+
+    <nav className="container">
+        <div>
+            <img src="/public/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+            <li href="#">Home</li>
+            <li href="#">About</li>
+            <li href="#">items</li>
+            <li href="#">Contact </li>
+            
+        </ul>
+        <button>login</button>
+    </nav>
+
+    )
+}
+
+export default Navbar;
